f516b1d7f64d1fca10124a78b9c64c1ae1fd2ae6
Elan Ruusamäe <glen@pld-linux.org>
2005-09-10 14:48:28 +0000

    - added
    Changed files:
        php-AutoIndex-config.patch -> 1.1
        php-AutoIndex.php -> 1.1

---
+++ php-AutoIndex.php
@@ -0,0 +1,31 @@
+<?php
+
+/* AutoIndex PHP Script config file
+
+base_dir	./
+icon_path	/AutoIndex/index_icons/winxp/
+language	en
+template	/usr/share/AutoIndex/templates/default/
+template_path	/AutoIndex/templates/default/
+log_file	false
+description_file	false
+user_list	.htpasswd.autoindex
+download_count	false
+hidden_files	/etc/AutoIndex/hidden_files
+banned_list	false
+show_dir_size	true
+use_login_system	false
+force_download	false
+search_enabled	true
+anti_leech	false
+must_login_to_download	false
+archive	false
+parse_htaccess	true
+days_new	0
+thumbnail_height	0
+bandwidth_limit	0
+md5_show	0
+
+*/
+
+?>
